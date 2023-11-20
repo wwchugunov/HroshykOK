bc795a8fbb31ed12f02db28ddd823bf919c8c527
wwchugunov <78274510+wwchugunov@users.noreply.github.com>
2023-11-20 16:19:49 +0300

    ва

--- js/index.js
+++ js/index.js
@@ -8,25 +8,27 @@ toggleButton.addEventListener('click', function () {
 
 
 const logo = document.querySelector('.logoheaderabout');
+const logoWidth = logo.clientWidth;
+const logoHeight = logo.clientHeight;
 
-function getRandomPosition() {
-    const maxX = window.innerWidth - logo.clientWidth;
-    const maxY = window.innerHeight - logo.clientHeight;
+function moveLogo(event) {
+    const mouseX = event.clientX;
+    const mouseY = event.clientY;
 
-    const randomX = Math.random() * maxX;
-    const randomY = Math.random() * maxY;
+    const maxX = window.innerWidth - logoWidth;
+    const maxY = window.innerHeight - logoHeight;
 
-    return { x: randomX, y: randomY };
+    const newX = Math.min(maxX, Math.max(0, mouseX - logoWidth / 2));
+    const newY = Math.min(maxY, Math.max(0, mouseY - logoHeight / 2));
+
+    logo.style.transition = 'transform 0.5s ease-in-out';
+    logo.style.transform = `translate(${newX}px, ${newY}px)`;
 }
 
-function moveLogo() {
-    const newPosition = getRandomPosition();
-    logo.style.transform = `translate(${newPosition.x}px, ${newPosition.y}px)`;
-}
-
-logo.addEventListener('mouseover', moveLogo);
-
-logo.addEventListener('transitionend', () => {
-    // Возвращаем логотип в центр экрана после завершения анимации
+function returnLogo() {
+    logo.style.transition = 'transform 2s ease-in-out';
     logo.style.transform = 'translate(-50%, -50%)';
-});
+}
+
+document.addEventListener('mousemove', moveLogo);
+document.addEventListener('mouseleave', returnLogo);
