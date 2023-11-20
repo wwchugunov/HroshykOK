2a5eaf1709457870447e6e408940795e97146d13
wwchugunov <78274510+wwchugunov@users.noreply.github.com>
2023-11-20 16:18:04 +0300

    7777

--- js/index.js
+++ js/index.js
@@ -9,17 +9,24 @@ toggleButton.addEventListener('click', function () {
 
 const logo = document.querySelector('.logoheaderabout');
 
-logo.addEventListener('mouseover', () => {
+function getRandomPosition() {
     const maxX = window.innerWidth - logo.clientWidth;
     const maxY = window.innerHeight - logo.clientHeight;
 
     const randomX = Math.random() * maxX;
     const randomY = Math.random() * maxY;
 
-    logo.style.transform = `translate(${randomX}px, ${randomY}px)`;
-});
+    return { x: randomX, y: randomY };
+}
+
+function moveLogo() {
+    const newPosition = getRandomPosition();
+    logo.style.transform = `translate(${newPosition.x}px, ${newPosition.y}px)`;
+}
+
+logo.addEventListener('mouseover', moveLogo);
 
 logo.addEventListener('transitionend', () => {
-    // Возвращаем логотип в верхнюю часть экрана после завершения анимации
-    logo.style.transform = 'translateX(-50%)';
+    // Возвращаем логотип в центр экрана после завершения анимации
+    logo.style.transform = 'translate(-50%, -50%)';
 });
