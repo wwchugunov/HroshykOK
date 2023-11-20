6d8ac84337981c313b41c77a2e4a1e4283e21206
wwchugunov <78274510+wwchugunov@users.noreply.github.com>
2023-11-20 11:55:55 +0200

    Update calculate.js

--- js/calculate.js
+++ js/calculate.js
@@ -1,39 +1,3 @@
-// function updateValue(sliderId, outputId, blockType) {
-//     const slider = document.getElementById(sliderId);
-//     const output = document.getElementById(outputId);
-//     const creditBodyValue = document.getElementById('creditBodyValue');
-//     const creditLineValue = document.getElementById('creditLineValue');
-//     const creditPayValue = document.getElementById('creditPayValue');
-
-//     output.innerText = slider.value;
-
-//     // Обновляем соответствующие элементы в каждом блоке
-//     if (blockType === 'creditinfoscoupe') {
-//         if (outputId === 'output1') {
-//             const creditAmount = parseFloat(slider.value);
-//             const numberOfDays = parseFloat(document.getElementById('output2').innerText);
-//             const annualInterestRate = 7; // 700% годовых
-//             const interestAmount = (creditAmount * annualInterestRate * numberOfDays) / 360;
-//             const totalAmount = creditAmount + interestAmount;
-//             // const totalAmount = creditBodyValue * (1 + annualInterestRate)
-
-//             creditBodyValue.innerText = slider.value + ' днів';
-//             creditPayValue.innerText = totalAmount.toFixed(2) + ' грн';
-//         } else if (outputId === 'output2') {
-//             creditLineValue.innerText = slider.value + ' днів';
-//             const creditAmount = parseFloat(document.getElementById('output1').innerText);
-//             const numberOfDays = parseFloat(slider.value);
-//             const annualInterestRate = 7; // 700% годовых
-//             const interestAmount = (creditAmount * annualInterestRate * numberOfDays) / 365;
-//             const totalAmount = creditAmount + interestAmount;
-
-//             creditPayValue.innerText = totalAmount.toFixed(2) + ' грн';
-//         }
-//     }
-// }
-
-
-
 function updateValue(sliderId, outputId, blockType) {
     const slider = document.getElementById(sliderId);
     const output = document.getElementById(outputId);
@@ -49,23 +13,25 @@ function updateValue(sliderId, outputId, blockType) {
             const creditAmount = parseFloat(slider.value);
             const numberOfDays = parseFloat(document.getElementById('output2').innerText);
             const annualInterestRate = 7; // 700% годовых
-            const dailyInterestRate = (1 + annualInterestRate / 100) ** (1 / 365);
-            const totalAmount = creditAmount * dailyInterestRate ** numberOfDays;
+            const interestAmount = (creditAmount * annualInterestRate * numberOfDays) / 360;
+            const totalAmount = creditAmount + interestAmount;
+            // const totalAmount = creditBodyValue * (1 + annualInterestRate)
 
             creditBodyValue.innerText = slider.value + ' днів';
-            creditPayValue.innerText = (totalAmount - creditAmount).toFixed(2) + ' грн';
+            creditPayValue.innerText = totalAmount.toFixed(2) + ' грн';
         } else if (outputId === 'output2') {
+            creditLineValue.innerText = slider.value + ' днів';
             const creditAmount = parseFloat(document.getElementById('output1').innerText);
             const numberOfDays = parseFloat(slider.value);
             const annualInterestRate = 7; // 700% годовых
-            const dailyInterestRate = (1 + annualInterestRate / 100) ** (1 / 365);
-            const totalAmount = creditAmount * dailyInterestRate ** numberOfDays;
+            const interestAmount = (creditAmount * annualInterestRate * numberOfDays) / 365;
+            const totalAmount = creditAmount + interestAmount;
 
-            creditLineValue.innerText = slider.value + ' днів';
-            creditPayValue.innerText = (totalAmount - creditAmount).toFixed(2) + ' грн';
+            creditPayValue.innerText = totalAmount.toFixed(2) + ' грн';
         }
     }
 }
+
 
 
 
